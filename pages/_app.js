ef5f8794067b20db320601ae479b444fa8c9b86f
sarvdavoudi <sarvdavoudi3000@yahoo.com>
2023-08-29 13:33:39 +0330

    fix: font fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 // import '../styles/reset.css'
 import '../styles/globals.css'
-import '../styles/fonts.css'
-import '../styles/theme.css'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
